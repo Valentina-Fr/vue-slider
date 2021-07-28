80a0f15a61788f795de9e871515da366adf6b57d
Valentina-Fr <v_frasca@virgilio.it>
2021-07-28 16:22:20 +0200

    Add backward/forward

--- js/script.js
+++ js/script.js
@@ -5,6 +5,26 @@ Vue.config.devtools = true;
 const root = new Vue({
     el: "#root",
     data: {
-        
+        currentIndex: 0,
+        images: [
+            "./img/image1.jpg",
+            "./img/image2.jpg",
+            "./img/image3.jpg",
+            "./img/image4.jpg"
+        ],
+    },
+    methods: {
+        isActive(index) {
+            return this.currentIndex === index ? "active" : "";
+        },
+
+        backward() {
+            this.currentIndex = this.currentIndex === 0 ? this.images.length - 1 : this.currentIndex - 1;
+        },
+
+        forward() {
+            this.currentIndex = this.currentIndex === this.images.length - 1 ? 0 : this.currentIndex + 1;
+        }
+
     }
 });
